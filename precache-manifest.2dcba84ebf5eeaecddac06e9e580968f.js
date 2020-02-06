c41978515823b8ee937dafcf68a4f587f9135599
Serhii Persianov <serhiipersianov@gmail.com>
2020-02-06 17:57:48 +0200

    Updates

---
+++ precache-manifest.2dcba84ebf5eeaecddac06e9e580968f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ae12412e183b75b3ef322598d217fb",
+    "url": "/test-wn/index.html"
+  },
+  {
+    "revision": "05f4cbde921546b6a5bd",
+    "url": "/test-wn/static/css/2.286c9144.chunk.css"
+  },
+  {
+    "revision": "5aff904b980d766c443e",
+    "url": "/test-wn/static/css/main.e6a8ce99.chunk.css"
+  },
+  {
+    "revision": "05f4cbde921546b6a5bd",
+    "url": "/test-wn/static/js/2.517d2cae.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/test-wn/static/js/2.517d2cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aff904b980d766c443e",
+    "url": "/test-wn/static/js/main.b878b8cb.chunk.js"
+  },
+  {
+    "revision": "d0a33c1af9a1a71ab121",
+    "url": "/test-wn/static/js/runtime-main.0c4a7257.js"
+  }
+]);
